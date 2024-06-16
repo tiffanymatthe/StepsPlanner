55a047867b89bbdb97f3256caccb16fca422cbeb
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-16 14:05:25 -0700

    make foot steps tighter

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -385,7 +385,7 @@ class Walker3DStepperEnv(EnvBase):
         # {self.max_curriculum + 1} levels in total
         dist_upper = np.linspace(*self.dist_range, self.max_curriculum + 1)
         dist_range = np.array([self.dist_range[0], dist_upper[self.curriculum]])
-        dist_range = dist_range * 0 + 0.45
+        dist_range = dist_range * 0 + 0.33
         yaw_range = self.yaw_range * ratio * DEG2RAD
         pitch_range = self.pitch_range * ratio * DEG2RAD + np.pi / 2
         tilt_range = self.tilt_range * ratio * DEG2RAD
@@ -402,7 +402,7 @@ class Walker3DStepperEnv(EnvBase):
         dphi[0] = 0.0
         dtheta[0] = np.pi / 2
 
-        dr[1:3] = self.init_step_separation
+        dr[1] = self.init_step_separation
         dphi[1:3] = 0.0
         dtheta[1:3] = np.pi / 2
 
