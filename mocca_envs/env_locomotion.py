5846b310e6b83cc10513f14bd4e09b78e391e5d6
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-16 22:38:00 -0700

    increase lift bonus and for longer

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -664,8 +664,8 @@ class Walker3DStepperEnv(EnvBase):
         abs_height = self.robot.body_xyz[2] - self.terrain_info[self.next_step_index, 2]
 
         self.contact_bonus = 0
-        if self.swing_leg_lifted and 1 <= self.swing_leg_lifted_count <= 10 and self._foot_target_contacts[self.swing_leg, 0] == 0:
-            self.contact_bonus += 0.1
+        if self.swing_leg_lifted and 1 <= self.swing_leg_lifted_count <= 50 and self._foot_target_contacts[self.swing_leg, 0] == 0:
+            self.contact_bonus += 0.5
 
         if abs(self.robot.body_rpy[2]) > 15 * DEG2RAD or abs(self.robot.lower_body_rpy[2]) > 15 * DEG2RAD:
             self.contact_bonus -= 1
