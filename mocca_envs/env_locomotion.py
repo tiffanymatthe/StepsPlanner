847ad638fc4efac462ba8d6c337c1499158ed972
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-18 18:40:00 -0700

    add heading to mirror

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -944,6 +944,7 @@ class Walker3DStepperEnv(EnvBase):
                 (
                     i * self.step_param_dim + 0,  # sin(-x) = -sin(x)
                     i * self.step_param_dim + 3,  # x_tilt
+                    i * self.step_param_dim + 5,
                 )
                 for i in range(self.lookahead + self.lookbehind)
             ],
