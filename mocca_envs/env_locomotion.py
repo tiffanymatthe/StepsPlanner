6fb24b77f168bdc6eee47cf87892197a24f79f8e
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-20 11:23:30 -0700

    penalize immediately

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -670,9 +670,8 @@ class Walker3DStepperEnv(EnvBase):
         if abs(self.robot.body_rpy[2]) > 15 * DEG2RAD or abs(self.robot.lower_body_rpy[2]) > 15 * DEG2RAD:
             self.contact_bonus -= 1
 
-        if self.curriculum > 0:
-            if np.abs(self.robot.feet_rpy[self.swing_leg, 1]) > 0.2:
-                self.contact_bonus -= np.abs(self.robot.feet_rpy[self.swing_leg, 1]) * 5
+        if np.abs(self.robot.feet_rpy[self.swing_leg, 1]) > 0.2:
+            self.contact_bonus -= np.abs(self.robot.feet_rpy[self.swing_leg, 1]) * 5
 
         # if self.swing_leg_has_fallen:
         #     print(f"{self.next_step_index}: swing leg has fallen, terminating")
@@ -760,8 +759,7 @@ class Walker3DStepperEnv(EnvBase):
         
         self.target_reached = self._foot_target_contacts[self.swing_leg, 0] > 0 and x_dist_to_target[self.swing_leg] < self.step_radius * 2 and y_dist_to_target[self.swing_leg] < self.step_radius and self.swing_leg_lifted
 
-
-        if self.curriculum > 0 and np.abs(self.robot.feet_rpy[self.swing_leg, 1]) > 0.2:
+        if np.abs(self.robot.feet_rpy[self.swing_leg, 1]) > 0.2:
             self.target_reached = False
 
         if self.target_reached:
