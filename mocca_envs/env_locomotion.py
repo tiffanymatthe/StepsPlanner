e76f09509aea5f3c8f41adf11d49d9c7dbb6b455
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-21 12:33:12 -0700

    add varied heading

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -432,6 +432,7 @@ class Walker3DStepperEnv(EnvBase):
         z = np.cumsum(dz)
 
         heading_targets = np.copy(dphi) + 90 * DEG2RAD
+        heading_targets[3:] += self.np_random.choice([0, -np.pi / 8, np.pi / 8, -np.pi / 4, np.pi / 4])
 
         return np.stack((x, y, z, dphi, x_tilt, y_tilt, heading_targets), axis=1)
 
