46944bf7f92cc6145508584486110c111f3b7e41
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-23 10:02:22 -0700

    only cap after 4th index

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -672,7 +672,9 @@ class Walker3DStepperEnv(EnvBase):
         self.distance_to_target = sqrt(ss(walk_target_delta[0:2]))
         foot_target_delta = self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg, 0:2]
         foot_distance_to_target = sqrt(ss(foot_target_delta[0:2]))
-        self.linear_potential = -(self.distance_to_target + min(foot_distance_to_target, 0.3)) / self.scene.dt
+        if self.next_step_index > 4:
+            foot_distance_to_target = min(foot_distance_to_target, 0.3)
+        self.linear_potential = -(self.distance_to_target + foot_distance_to_target * 0.5) / self.scene.dt
 
         # walk_target_delta = self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg, 0:2]
         # self.distance_to_target = sqrt(ss(walk_target_delta[0:2]))
