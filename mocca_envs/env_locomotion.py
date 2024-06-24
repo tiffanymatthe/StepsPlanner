f7576cc9e4ad99b3dcee98ba0a86151158773123
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-24 14:03:49 -0700

    only stop every other step

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -330,7 +330,7 @@ class Walker3DStepperEnv(EnvBase):
     lookbehind = 1
     walk_target_index = -1
     step_bonus_smoothness = 1
-    stop_steps = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+    stop_steps = [3,5,7,9,11,13,15,17,19]
 
     def __init__(self, **kwargs):
         # Handle non-robot kwargs
