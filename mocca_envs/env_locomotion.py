1b46bc9e73f992cf1d86093c95d13659149c5193
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-25 13:31:11 -0700

    add z component in bonus

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -671,10 +671,10 @@ class Walker3DStepperEnv(EnvBase):
 
         walk_target_delta = self.walk_target - self.robot.body_xyz
         body_distance_to_target = sqrt(ss(walk_target_delta[0:2]))
-        foot_target_delta = self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg, 0:2]
+        foot_target_delta = self.terrain_info[self.next_step_index, 0:3] - self.robot.feet_xyz[self.swing_leg, 0:3]
         foot_distance_to_target = sqrt(ss(foot_target_delta[0:2]))
         self.linear_potential = -(body_distance_to_target + foot_distance_to_target * 0.5) / self.scene.dt
-        self.distance_to_target = foot_distance_to_target
+        self.distance_to_target = sqrt(ss(foot_target_delta[0:3]))
         # walk_target_delta = self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg, 0:2]
         # self.distance_to_target = sqrt(ss(walk_target_delta[0:2]))
         # self.linear_potential = -self.distance_to_target / self.scene.dt
