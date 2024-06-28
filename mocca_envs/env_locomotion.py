4a98b19c208de376a390c0218767916f800bf578
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-06-27 21:16:48 -0700

    switch to body potential

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -699,7 +699,7 @@ class Walker3DStepperEnv(EnvBase):
         foot_target_delta = self.terrain_info[self.next_step_index, 0:3] - self.robot.feet_xyz[self.swing_leg, 0:3]
         foot_distance_to_target = sqrt(ss(foot_target_delta[0:2]))
         self.linear_potential = -(body_distance_to_target + foot_distance_to_target * 0.5) / self.scene.dt
-        self.distance_to_target = (sqrt(ss(foot_target_delta[0:3])) + body_distance_to_target) / 2
+        self.distance_to_target = body_distance_to_target
         # walk_target_delta = self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg, 0:2]
         # self.distance_to_target = sqrt(ss(walk_target_delta[0:2]))
         # self.linear_potential = -self.distance_to_target / self.scene.dt
