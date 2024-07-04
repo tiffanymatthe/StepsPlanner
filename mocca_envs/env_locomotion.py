8544bfa134ee32859a84079ca8cbbd03c4f8ac38
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-04 12:12:01 -0700

    only do heading bonus once target is reached;

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -933,7 +933,8 @@ class Walker3DStepperEnv(EnvBase):
         if self.body_stationary_count > count:
             self.legs_bonus -= 100
 
-        self.heading_bonus = np.exp(-0.5 * abs(self.heading_rad_to_target) **2)
+        if self.target_reached:
+            self.heading_bonus = np.exp(-0.5 * abs(self.heading_rad_to_target) **2)
 
         self.done = self.done or self.tall_bonus < 0 or abs_height < -3 or self.swing_leg_has_fallen or self.other_leg_has_fallen or self.body_stationary_count > count
         # if self.done:
