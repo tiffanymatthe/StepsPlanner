5060e8d04ce97be69ca42dc3fac79ada1e6361a6
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-04 08:18:21 -0700

    make every step stop;

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -328,7 +328,7 @@ class Walker3DStepperEnv(EnvBase):
     lookbehind = 1
     walk_target_index = -1
     step_bonus_smoothness = 1
-    stop_steps = [6, 7, 13, 14]
+    stop_steps = list(range(4,20)) #  [6, 7, 13, 14]
 
     def __init__(self, **kwargs):
         # Handle non-robot kwargs
