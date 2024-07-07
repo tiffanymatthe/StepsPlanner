2fcfc63846264b34e270089ff4103522cd847bd9
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-07 09:28:00 -0700

    make deterministic

--- common/sacred_utils.py
+++ common/sacred_utils.py
@@ -71,3 +71,6 @@ def seed_all(seed):
     random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = False
