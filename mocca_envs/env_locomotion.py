110463f58cd7e277482ad4dbc9dd0301f0f41ee4
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-18 18:59:56 -0700

    remove stop steps

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -330,7 +330,7 @@ class Walker3DStepperEnv(EnvBase):
     lookbehind = 1
     walk_target_index = -1
     step_bonus_smoothness = 1
-    stop_steps = [4, 5, 13, 14, 18, 19] # list(range(4,20))
+    stop_steps = [18, 19] # list(range(4,20))
 
     def __init__(self, **kwargs):
         # Handle non-robot kwargs
