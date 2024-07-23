4e58345c72b4efb63397ac0a5ec0e4e0ae5d2510
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-23 09:04:43 -0700

    no standstill, just make faster

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -354,7 +354,7 @@ class Walker3DStepperEnv(EnvBase):
         self.allow_backward_switch = False
         self.allow_double_step = False
         self.for_and_back = False
-        self.to_standstill = True
+        self.to_standstill = False
         self.heading_bonus_weight = kwargs.pop("heading_bonus_weight", 1)
         self.gauss_width = kwargs.pop("gauss_width", 0.5)
         self.tilt_bonus_weight = 1
@@ -595,8 +595,10 @@ class Walker3DStepperEnv(EnvBase):
 
         # # factor = 2
         # timing_counts += self.np_random.choice([-0.5,-0.3,0,0.5,1.0]) * timing_counts
-        # # if self.curriculum > 0:
-        # #     timing_counts += -timing_counts * 0.5
+        if self.curriculum == 1:
+            timing_counts += -timing_counts * self.np_random.choice([0,0.3])
+        if self.curriculum > 1:
+            timing_counts += -timing_counts * self.np_random.choice([0,0.3,0.5])
 
         timing_counts[0] = 10
         timing_counts[1] = 10
