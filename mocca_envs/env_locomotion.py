5c7ead5c68845bf5339332d3df70f0e813a8ca34
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-24 15:05:29 -0700

    do not zero out, just copy

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1244,8 +1244,8 @@ class Walker3DStepperEnv(EnvBase):
         # should angles be per feet? yes so it doesn't change too much
         feet_heading = np.array([self.robot.feet_rpy[int(i), 2] for i in targets[:, 7]])
         heading_angle_to_targets = targets[:, 6] - feet_heading
-        # heading_angle_to_targets[0] = 0
-        # heading_angle_to_targets[2] = 0
+        heading_angle_to_targets[0] = heading_angle_to_targets[1]
+        heading_angle_to_targets[2] = heading_angle_to_targets[1]
         # heading_angle_to_targets = targets[:, 6] - self.robot.body_rpy[2]
 
         swing_legs_at_targets = np.where(targets[:, 7] == 0, -1, 1)
