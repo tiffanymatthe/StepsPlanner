710a86f03e77ff6845c4a5d2a14e7d5a9081b6d8
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-07-27 08:18:47 -0700

    check foot angle pot all the time

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1214,9 +1214,9 @@ class Walker3DStepperEnv(EnvBase):
             self.linear_potential = -(body_distance_to_target) / self.scene.dt
             self.distance_to_target = body_distance_to_target
 
-        if self._foot_target_contacts[self.swing_leg, 0] == 0:
-            angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
-            self.linear_potential += -(angle_delta * 0.1) / self.scene.dt
+        # if self._foot_target_contacts[self.swing_leg, 0] == 0:
+        angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
+        self.linear_potential += -(angle_delta * 0.1) / self.scene.dt
 
     def calc_base_reward(self, action):
 
