9244851e4930b71b8ed3c81bbeb353e2ae69edfe
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-08-03 18:20:59 -0700

    add timing met and reduce elbow to 0.4

--- playground/enjoy.py
+++ playground/enjoy.py
@@ -226,7 +226,7 @@ def main():
                     expected_other_foot = []
                     actual_start_foot = []
                     actual_other_foot = []
-                print(f"--- Episode reward: {ep_reward} and average heading error: {nanmean(env.heading_errors) * RAD2DEG:.2f} deg and timing acc: {nanmean(env.timing_count_errors):.2f}")
+                print(f"--- Episode reward: {ep_reward} and average heading error: {nanmean(env.heading_errors) * RAD2DEG:.2f} deg and timing acc: {nanmean(env.timing_count_errors):.2f}, {nanmean(env.met_times):.2f}")
                 obs = env.reset(reset_runner=False)
                 if args.heading:
                     foot_heading_targets = env.terrain_info[:, 6]
