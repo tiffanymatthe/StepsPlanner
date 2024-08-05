921574836f86980e0b81e717a58d520bfd917757
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-08-05 14:02:22 -0700

    fix bug

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1128,7 +1128,7 @@ class Walker3DStepperEnv(EnvBase):
         self.linear_potential = -(body_distance_to_target) / self.scene.dt
         self.distance_to_target = body_distance_to_target
 
-        if self.behaviors[self.selected_behavior] not in {"to_standstill", "random_walks"}:
+        if self.selected_behavior not in {"to_standstill", "random_walks"}:
             angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
             self.linear_potential += -(angle_delta * 0.2) / self.scene.dt
 
