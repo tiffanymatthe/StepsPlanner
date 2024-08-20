236b5053b6fa046edfe377d80d38f3d9b4c63463
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-08-20 11:10:42 -0700

    fix some resetting and allow more falling

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -383,7 +383,7 @@ class Walker3DStepperEnv(EnvBase):
 
         # Robot settings
         N = self.max_curriculum + 1
-        self.terminal_height_curriculum = np.linspace(1.03, 1.03, N)
+        self.terminal_height_curriculum = np.linspace(0.8, 0.8, N)
         self.applied_gain_curriculum = np.linspace(1.2, 1.2, N)
         self.electricity_cost = 4.5 / self.robot.action_space.shape[0]
         self.stall_torque_cost = 0.225 / self.robot.action_space.shape[0]
@@ -1411,6 +1411,7 @@ class Walker3DStepperEnv(EnvBase):
         self.body_stationary_count = 0
 
         self.current_step_time = 0
+        self.current_time_index = 1
 
         self.heading_errors = []
         self.met_times = []
