e2581475049fdc588afaf9757db06a8e23dd60fd
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-08-20 17:13:37 -0700

    increase potential to 0.3

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1532,7 +1532,7 @@ class Walker3DStepperEnv(EnvBase):
 
         angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
 
-        self.linear_potential = -(body_distance_to_target + angle_delta * 0.1) / self.scene.dt
+        self.linear_potential = -(body_distance_to_target + angle_delta * 0.3) / self.scene.dt
         self.distance_to_target = body_distance_to_target
 
     def calc_base_reward(self, action):
