b0bd844d5947db545f4656c14063fef8a899c9cd
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-10-05 15:52:00 -0700

    multiplier always 2

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1740,7 +1740,7 @@ class Walker3DStepperEnv(EnvBase):
 
         angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
 
-        multiplier = 2 if (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net) else 0.1
+        multiplier = 2 # if (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net) else 0.1
 
         if self.mask_info["heading"][2]:
             multiplier = 0
