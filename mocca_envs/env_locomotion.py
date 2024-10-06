5581f63f736b99460697476c21cc1c0d2a659476
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-10-06 07:11:33 -0700

    cut half

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -323,7 +323,7 @@ class Walker3DStepperEnv(EnvBase):
     rendered_step_count = 4
     init_step_separation = 0.70
 
-    step_delay = 6
+    step_delay = 4
 
     lookahead = 2
     lookbehind = 1
@@ -1933,9 +1933,9 @@ class Walker3DStepperEnv(EnvBase):
 
         self.heading_rad_to_target = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
         if self.next_step_index > 1:
-            self.prev_heading_rad_to_target = self.smallest_angle_between(self.robot.feet_rpy[1-self.swing_leg,2], self.terrain_info[self.next_step_index-1, 6])
-            # prev_index = np.where(self.terrain_info[0:self.next_step_index, 7] == 1-self.swing_leg)[0][-1]
-            # self.prev_heading_rad_to_target = self.smallest_angle_between(self.robot.feet_rpy[1-self.swing_leg,2], self.terrain_info[prev_index, 6])
+            # self.prev_heading_rad_to_target = self.smallest_angle_between(self.robot.feet_rpy[1-self.swing_leg,2], self.terrain_info[self.next_step_index-1, 6])
+            prev_index = np.where(self.terrain_info[0:self.next_step_index, 7] == 1-self.swing_leg)[0][-1]
+            self.prev_heading_rad_to_target = self.smallest_angle_between(self.robot.feet_rpy[1-self.swing_leg,2], self.terrain_info[prev_index, 6])
         else:
             self.prev_heading_rad_to_target = 0
 
