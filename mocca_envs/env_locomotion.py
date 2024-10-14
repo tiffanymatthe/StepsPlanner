fa66118638a1ac5be4ea772b6b3ed723f7aeca74
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-10-14 11:33:21 -0700

    Revert "add back older rewards"
    This reverts commit 9b73cf9d214aceaa84738ac7430567483d94a455.

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -1740,16 +1740,16 @@ class Walker3DStepperEnv(EnvBase):
 
         angle_delta = self.smallest_angle_between(self.robot.feet_rpy[self.swing_leg,2], self.terrain_info[self.next_step_index, 6])
 
-        multiplier = 2 if (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net) else 0.1
+        multiplier = 2 # if (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net) else 0.1
 
         if self.mask_info["heading"][2]:
             multiplier = 0
 
-        if self.mask_info["timing"][2] and self.next_step_index <= 2: # and not (self.curriculum > 0 or self.behavior_curriculum > 0):
-            # add a foot distance potential if there is no timing signal
-            foot_delta = sqrt(ss(self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg][0:2])) * 0.3
-        else:
-            foot_delta = 0
+        # if self.mask_info["timing"][2] and self.next_step_index <= 2: # and not (self.curriculum > 0 or self.behavior_curriculum > 0):
+        #     # add a foot distance potential if there is no timing signal
+        #     foot_delta = sqrt(ss(self.terrain_info[self.next_step_index, 0:2] - self.robot.feet_xyz[self.swing_leg][0:2])) * 0.3
+        # else:
+        #     foot_delta = 0
         foot_delta = 0
 
         self.linear_potential = -(body_distance_to_target + angle_delta * multiplier + foot_delta) / self.scene.dt
@@ -1813,8 +1813,8 @@ class Walker3DStepperEnv(EnvBase):
         else:
             self.body_stationary_count = 0
         count = 200
-        if self.body_stationary_count > count:
-            self.legs_bonus -= 100
+        # if self.body_stationary_count > count:
+        #     self.legs_bonus -= 100
 
         if self.mask_info["heading"][2]:
             self.heading_bonus = 0
@@ -1824,7 +1824,7 @@ class Walker3DStepperEnv(EnvBase):
             else:
                 self.heading_bonus = 0
 
-            if self.current_step_time <= self.terrain_info[self.next_step_index, 10] and self.next_step_index > 1 and (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net):
+            if not self.mask_info["timing"][2] and self.current_step_time <= self.terrain_info[self.next_step_index, 10] and self.next_step_index > 1 and (self.curriculum > 0 or self.behavior_curriculum > 0 or self.from_net):
                 self.heading_bonus += -( -np.exp(-self.gauss_width * abs(self.prev_heading_rad_to_target) ** 2) + 1) * 0.5
         
         if self.mask_info["timing"][2]:
@@ -1983,7 +1983,7 @@ class Walker3DStepperEnv(EnvBase):
             self.terrain_info[self.next_step_index, 10],
             self.terrain_info[self.next_step_index, 11]
         ]
-        if (self.target_reached and self.next_step_index > 2 and self.current_step_time < next_step_time[0] + next_step_time[1]):
+        if not self.mask_info["timing"][2] and (self.target_reached and self.next_step_index > 2 and self.current_step_time < next_step_time[0] + next_step_time[1]):
             self.target_reached = False
 
         self.past_last_step = self.past_last_step or (self.reached_last_step and self.target_reached_count >= 2)
