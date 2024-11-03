5d23fea18c5fef7449a6c5333e880e5476d1a1c7
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-03 09:36:00 -0800

    add back current iteration wait

--- playground/train.py
+++ playground/train.py
@@ -382,7 +382,7 @@ def main(_seed, _config, _run):
 
         # Update curriculum after roll-out
         if (
-            update_curriculum # and current_iteration >= 50
+            update_curriculum and current_iteration >= 50
         ):
             current_iteration = 0
             if current_curriculum < max_curriculum:
