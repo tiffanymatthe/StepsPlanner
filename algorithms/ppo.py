5825c73510759ce19a6bebd8e7201e4c0d52c25c
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-08 06:59:28 -0800

    Revert "add freezing"
    This reverts commit aac4b6930dd7fdb77fd39009bc0b54d9c66e7b05.

--- algorithms/ppo.py
+++ algorithms/ppo.py
@@ -145,7 +145,7 @@ class PPO(object):
                 device=device,
             )
 
-    def update(self, rollouts, only_update_critic=False):
+    def update(self, rollouts):
         advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
 
@@ -163,7 +163,7 @@ class PPO(object):
         clip_param = self.clip_param
 
         parameters = [
-            p for p in self.actor_critic.parameters() if p.requires_grad is not None and p.requires_grad
+            p for p in self.actor_critic.parameters() if p.requires_grad is not None
         ]
         assert len(parameters) != 0, "No trainable parameters"
 
@@ -225,7 +225,6 @@ class PPO(object):
                     + action_loss
                     - dist_entropy * self.entropy_coef
                 ).backward()
-
                 clip_grad_norm_(parameters, self.max_grad_norm)
                 self.optimizer.step()
 
@@ -233,18 +232,12 @@ class PPO(object):
                 self.optimizer.zero_grad()
                 if self.normal_gnt:
                     critic_fraction_to_replace, dormant_critic_count, dormant_critic_fraction = self.critic_gnt.gen_and_test(features=self.actor_critic.get_activations(), only_test=self.only_test)
-                    if not only_update_critic:
-                        actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = self.actor_gnt.gen_and_test(features=self.actor_critic.actor.get_activations(), only_test=self.only_test)
-                    else:
-                        actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = 0,0,0
+                    actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = self.actor_gnt.gen_and_test(features=self.actor_critic.actor.get_activations(), only_test=self.only_test)
                 else:
                     critic_history = torch.stack(self.actor_critic.get_activations()).permute(1, 0, 2)
+                    actor_history = torch.stack(self.actor_critic.actor.get_activations()).permute(1, 0, 2)
                     critic_fraction_to_replace, dormant_critic_count, dormant_critic_fraction = self.critic_gnt.gen_and_test(features=critic_history, only_test=self.only_test)
-                    if not only_update_critic:
-                        actor_history = torch.stack(self.actor_critic.actor.get_activations()).permute(1, 0, 2)
-                        actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = self.actor_gnt.gen_and_test(features=actor_history, only_test=self.only_test)
-                    else:
-                        actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = 0,0,0
+                    actor_fraction_to_replace, dormant_actor_count, dormant_actor_fraction = self.actor_gnt.gen_and_test(features=actor_history, only_test=self.only_test)
 
                 value_loss_epoch.add_(value_loss.detach())
                 action_loss_epoch.add_(action_loss.detach())
