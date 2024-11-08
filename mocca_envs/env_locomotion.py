738f1eae9edb1625bd541ca5a2721ff0dcbd4443
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-07 22:19:12 -0800

    make turn in place start from not a standstill

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -408,7 +408,7 @@ class Walker3DStepperEnv(EnvBase):
             "to_standstill": np.linspace(np.pi / 12, np.pi / 3, N),
             "random_walks": np.linspace(np.pi / 12, np.pi / 3, N),
             "random_walks_backward": np.linspace(np.pi / 12, np.pi / 3, N),
-            "turn_in_place": np.linspace(0, np.pi / 2, N),
+            "turn_in_place": np.linspace(np.pi/12, np.pi / 2, N),
             "side_step": None,
             "backward": np.linspace(np.pi / 12, np.pi / 4, N),
             "heading_var": np.linspace(0, np.pi / 3 - np.pi / 8, N),
