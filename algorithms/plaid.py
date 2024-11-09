315b42ce4137dbe2aaabdc5f546d2a8703f4cac9
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-08 17:07:56 -0800

    try modifying distiller a bit

--- algorithms/plaid.py
+++ algorithms/plaid.py
@@ -141,7 +141,8 @@ class Distiller:
 
                         if not use_expert:
                             # determines if we get observations from the student or teacher, but reference data is from teacher for MSE loss calc
-                            _, student_action, _ = current_expert_policy.act(self.buffer_observations_per_task[task_i][buffer_index], deterministic=(np.random.rand() < epoch / num_epochs))
+                            student_deterministic = np.random.rand() < min(epoch / 20, 1)
+                            _, student_action, _ = current_expert_policy.act(self.buffer_observations_per_task[task_i][buffer_index], deterministic=student_deterministic)
 
                         if use_expert:
                             cpu_actions = expert_action.cpu().numpy()
