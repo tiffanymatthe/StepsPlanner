cf93d4ee2199f0470345f8d1503656776d334053
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-12 10:37:23 -0800

    adjust plaid tolerance

--- algorithms/plaid.py
+++ algorithms/plaid.py
@@ -277,7 +277,7 @@ class Distiller:
                 )
             )
 
-            if ep_action_loss.item() <= 0.001 and epoch > 11:
+            if ep_action_loss.item() <= 0.02 and epoch > 11:
                 print("Quitting early.")
                 break
 
