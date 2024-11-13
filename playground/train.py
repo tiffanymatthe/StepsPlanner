3a498f68fe107e716ac64e14b8c2ef7b0d03b92a
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-13 12:48:07 -0800

    increase heading threshold to 10

--- playground/train.py
+++ playground/train.py
@@ -368,7 +368,7 @@ def main(_seed, _config, _run):
                     )
                     and (
                         np.isnan(avg_heading_err_nanmean)
-                        or avg_heading_err_nanmean < (7 * DEG2RAD if (current_curriculum > 0 or args.net is not None) else 25 * DEG2RAD)
+                        or avg_heading_err_nanmean < (10 * DEG2RAD if (current_curriculum > 0 or args.net is not None) else 25 * DEG2RAD)
                         or (avg_heading_err_nanmean < 11 * DEG2RAD and current_behavior_curriculum == 5 and current_curriculum == 9)
                     )
                     and (
