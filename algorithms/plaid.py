f5c84d997e98cda2353e13c5968fd91c782ca3a6
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-13 07:25:47 -0800

    lower threshold to 0.01

--- algorithms/plaid.py
+++ algorithms/plaid.py
@@ -277,7 +277,7 @@ class Distiller:
                 )
             )
 
-            if ep_action_loss.item() <= 0.02 and epoch > 50:
+            if ep_action_loss.item() <= 0.01 and epoch > 50:
                 print("Quitting early.")
                 break
 
