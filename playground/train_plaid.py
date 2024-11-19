13fa407cc240c9036d8894b6d8a9d813a43631af
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-18 18:17:33 -0800

    5000 steps, 500 epochs

--- playground/train_plaid.py
+++ playground/train_plaid.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         device=device,
         num_processes=10,
         num_experts=len(experts),
-        num_epochs=1000,
+        num_epochs=500,
         dummy_env=dummy_env,
         log_dir=save_dir,
     )
