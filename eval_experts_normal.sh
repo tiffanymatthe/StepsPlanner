499a2d01269450a8476a3ed878e0dbb68404b94b
Tiffany Matthe <tiffanymatthe@gmail.com>
2024-11-21 21:29:12 -0800

    eval experts

---
+++ eval_experts_normal.sh
@@ -0,0 +1,212 @@
+set -e
+
+# Define the ranges and special conditions
+behavior_start=0   # Starting value for behavior_curriculum
+behavior_end=2     # Ending value for behavior_curriculum
+a=0                # Starting value for curriculum in the first behavior_curriculum
+b=0                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_14/plasticity_elaho/models//Walker3DStepperEnv-v0_curr_${behavior_curriculum}_${curriculum}.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
+
+# Define the ranges and special conditions
+behavior_start=2   # Starting value for behavior_curriculum
+behavior_end=2     # Ending value for behavior_curriculum
+a=1                # Starting value for curriculum in the first behavior_curriculum
+b=1                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_15/plasticity_elaho_cont_lowered_threshold/models/Walker3DStepperEnv-v0_curr_${behavior_curriculum}_${curriculum}.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
+
+
+# Define the ranges and special conditions
+behavior_start=2   # Starting value for behavior_curriculum
+behavior_end=5     # Ending value for behavior_curriculum
+a=2                # Starting value for curriculum in the first behavior_curriculum
+b=8                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_15/plasticity_elaho_cont_lowered_threshold_fixed/models/Walker3DStepperEnv-v0_curr_${behavior_curriculum}_${curriculum}.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
+
+# Define the ranges and special conditions
+behavior_start=5   # Starting value for behavior_curriculum
+behavior_end=9     # Ending value for behavior_curriculum
+a=9                # Starting value for curriculum in the first behavior_curriculum
+b=8                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_18/plasticity_elaho_cont_lowered_threshold_fixed_cont/models/Walker3DStepperEnv-v0_curr_${behavior_curriculum}_${curriculum}.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
+
+# Define the ranges and special conditions
+behavior_start=9   # Starting value for behavior_curriculum
+behavior_end=9     # Ending value for behavior_curriculum
+a=9                # Starting value for curriculum in the first behavior_curriculum
+b=9                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_18/plasticity_elaho_cont_lowered_threshold_fixed_cont/models/Walker3DStepperEnv-v0_600000000.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
+
+# Define the ranges and special conditions
+behavior_start=10   # Starting value for behavior_curriculum
+behavior_end=10     # Ending value for behavior_curriculum
+a=0                # Starting value for curriculum in the first behavior_curriculum
+b=8                # Ending value for curriculum in the last behavior_curriculum
+
+for behavior_curriculum in $(seq $behavior_start $behavior_end); do
+    if [ $behavior_curriculum -eq $behavior_start ]; then
+        # For the first behavior_curriculum
+        curriculum_start=$a
+        curriculum_end=9
+    elif [ $behavior_curriculum -eq $behavior_end ]; then
+        # For the last behavior_curriculum
+        curriculum_start=0
+        curriculum_end=$b
+    else
+        # For all other behavior_curriculum values
+        curriculum_start=0
+        curriculum_end=9
+    fi
+
+    for curriculum in $(seq $curriculum_start $curriculum_end); do
+        echo "Running $behavior_curriculum - $curriculum"
+        python3 -m playground.enjoy \
+            --env Walker3DStepperEnv-v0 \
+            --net "runs/dream/oct_19/plasticity_elaho_cont_one_step_plant/models/Walker3DStepperEnv-v0_curr_${behavior_curriculum}_${curriculum}.pt" \
+            --plank_class VeryLargePlank \
+            --curriculum "$curriculum" \
+            --behavior_curriculum "$behavior_curriculum" \
+            --determine \
+            --render 0 \
+            --plot 0
+    done
+done
--- playground/enjoy.py
+++ playground/enjoy.py
@@ -360,7 +360,7 @@ def main():
 
         rows = zip(timing_mets, heading_errs, dist_errs, curriculum_metrics)
 
-        with open(f"all_with_hopping/data_{behavior_curriculum}_{curriculum}.csv", "w") as f:
+        with open(f"all_expert_baseline/data_{behavior_curriculum}_{curriculum}.csv", "w") as f:
             writer = csv.writer(f)
             writer.writerow(["timing_met", "heading_err", "dist_err", "curriculum_metric"])
             for row in rows:
