6213637fc24e5d05d6a9d31a6000c588dd1bcc3d
Tiffany Matthe <tiffanymatthe@gmail.com>
2025-03-31 22:43:37 -0700

    train backwards

--- mocca_envs/env_locomotion.py
+++ mocca_envs/env_locomotion.py
@@ -336,7 +336,7 @@ class Walker3DStepperEnv(EnvBase):
         self.max_curriculum = 9
         self.advance_threshold = 12  # steps_reached
 
-        self.task = 4
+        self.task = 2
         self.max_task = 4
 
         # Robot settings
